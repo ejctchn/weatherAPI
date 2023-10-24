5545fe233a7d398731581dc84426a749c244ad35
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2023-10-23 22:55:52 -0600

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+const app = document.getElementById('app');
+
+// const header = document.createElement('h1');
+
+// const headerContent = document.createTextNode('Develop, Preview, Ship');
+
+// header.appendChild(headerContent);
+
+// app.appendChild(header);
+
+function Header()
+{
+    return (
+        <h1>
+            Develop. Preview. Ship.
+        </h1>
+    );
+}
+function HomePage() {
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+    const [likes, setLikes] = React.useState(0);
+  
+    function handleClick()
+    {
+        setLikes(likes + 1);
+    }
+    return (
+      <div>
+        <Header title="Develop. Preview. Ship. 🚀" />
+        <ul>
+            {names.map((name) => 
+            (
+                <li key={name}>{name}</li>
+            ))}
+        </ul>
+  
+        <button onClick={handleClick}>Like</button>
+      </div>
+    );
+  }
+ReactDOM.render(<HomePage />, app);
