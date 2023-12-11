c39853c34f3532d98e310d1d8bd35886a98cb7b7
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2023-12-10 17:06:30 -0700

    Delete cursor-animation.ts

--- src/app/cursor-animation.ts
+++
@@ -1,8 +0,0 @@
-import { gsap } from 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.3/gsap.min.js';
- 
-gsap.set('.cursor',{xPercent:-50, yPercent: -50});
-let cursor = document.querySelector('.cursor');
-let title = document.querySelector('h1');
-
-let mouseX;
-let mouseY;
